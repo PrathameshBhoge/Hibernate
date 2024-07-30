953ef4a0856e16bc39c83ba56e7fb6edfb84b9b4
PrathameshBhoge <prathameshbhoge230@gmail.com>
2024-07-30 20:21:02 +0530

    Initial commit

--- src/main/java/com/hibernates/student/Student.java
+++ src/main/java/com/hibernates/student/Student.java
@@ -12,9 +12,7 @@ public class Student {
     private int id;
     private String name;
     private String city;
-
     private Certificate certificate;
-
     public Certificate getCertificate() {
         return certificate;
     }
